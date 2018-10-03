e6894978ba7b17ba22a3f07260a18dde9facd145
alireza <ali@Alirezas-MacBook-Air.local>
2018-10-02 22:38:50 -0700

    latest fix

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # serverHealth
+This script is very useful to just to see get status/infromation about your host and it's certificate
--- get_site_details.py
+++ get_site_details.py
@@ -1,32 +1,40 @@
 #! /usr/bin/env python
-"""Check Server Health"""
+"""This script is very useful to just to see get status/infromation about your host and it's certificate"""
 from urllib.request import Request, urlopen, ssl, socket
 from urllib.error import URLError, HTTPError
-import os,json
+import os,json,hashlib, re
+
 
 class ServerHealthCheck():
 
     def __init__(self, base_url, port, tcp):
-        self.base_url="www."+base_url
+        self.base_url=base_url
+        self.ip_now = self.obtain_ip()
         self.port=port
         self.tcp=tcp
-        self.url_path = self.tcp+"://"+self.base_url	
+        self.url_path = self.tcp+"://"+base_url
         self.ping_host()
         self.obtain_http_info()
         self.obtain_cert_info()
+        
+
+    def obtain_ip(self):
+    	currnet_ip = socket.gethostbyname(self.base_url)
+    	print("ip: "+currnet_ip)
+    	return currnet_ip
 
     def ping_host(self):
         #ping reesult
-        print("PING INFO ....................................")
-        response = os.system("ping -c 1 " + self.base_url)        
+        print("\n\n"+"__PING INFO____________________________________________")
+        response = os.system("ping -c 1 " + self.ip_now)        
         #and then check the response...
         if response == 0:
-            print(self.base_url, 'is up!')
+            print("server "+ self.base_url+": is up ")
         else:
-            print(hostname, 'is down!')
+             print("server "+ self.base_url+": is DOWN !!!")
 
     def obtain_http_info(self):
-        print("SSL/TLS INFO ....................................")
+        print("__SSL/TLS INFO____________________________________________")
         
         req = Request(self.url_path)
         try:
@@ -42,26 +50,53 @@ class ServerHealthCheck():
             print("http code:"+str(response.getcode())  )
             
     def obtain_cert_info(self):
+
         context = ssl.create_default_context()
         with socket.create_connection((self.base_url, self.port)) as socket_connection:
             with context.wrap_socket(socket_connection, server_hostname=self.base_url) as server_socket:
-                print(server_socket.version())
-                print(json.dumps(server_socket.getpeercert() , indent=2, sort_keys=True))
+                #uncomment to print everything
+                #print(json.dumps(server_socket.getpeercert() , indent=2, sort_keys=True))
+                
+                cert_info = server_socket.getpeercert()
+                subject = dict(x[0] for x in cert_info['subject'])
+                issued_to = subject['commonName']
+                issuer = dict(x[0] for x in cert_info['issuer'])
+                issued_by = issuer['commonName']
+                valid_from =cert_info['notBefore']
+                valid_to = cert_info['notAfter']
+                serial_number =cert_info['serialNumber']
+
+              
+
                 der_cert = server_socket.getpeercert(False)
                 der_cert_bin = server_socket.getpeercert(True)
+
                 pem_cert = ssl.DER_cert_to_PEM_cert(server_socket.getpeercert(True))
-                print(pem_cert)
+                # uncomment the below line if you want to see the actual public cert
+                #print("certificate pub:",pem_cert)
+                thumb_md5 = hashlib.md5(der_cert_bin).hexdigest()
+                thumb_sha1 = hashlib.sha1(der_cert_bin).hexdigest()
+                thumb_sha256 = hashlib.sha256(der_cert_bin).hexdigest()
+                print("issued_to: " + issued_to)
+                print("issued_by: " + issued_by)
+                print("valid_from: " + valid_from)
+                print("valid_to: " + valid_from)
+                print("MD5: " + thumb_md5)
+                print("SHA1: " + thumb_sha1)
+                print("SHA256: " + thumb_sha256)                
                 print ("cipher: "+str(server_socket.cipher()))
                 print("SSL/TLS version:  "+server_socket.version())
-                print(server_socket.shared_ciphers())
+                print("serial_number: "+serial_number)
+                # print(server_socket.shared_ciphers())
             server_socket.close()
 
 
 
-
-
-
-host_name = "google.com"
-prt = 443
-tcp_it = "https"
-serverHealthCheck = ServerHealthCheck(host_name, prt, tcp_it)
+if __name__ == '__main__':
+	# DO NOT USE IP
+    
+    host_name = input("host name ? (example github.com) \n")
+  
+    prt = input("port ? \n")
+    tcp_it = "https"
+    serverHealthCheck = ServerHealthCheck(host_name, prt, tcp_it)
